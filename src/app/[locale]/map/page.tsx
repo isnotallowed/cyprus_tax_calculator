63185f2688678630ff616416bdb80028345669dc
Alexander <goosehustle@gmail.com>
2024-06-10 07:35:05 +0400

    chore: map

---
+++ src/app/[locale]/map/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix default icon issue in Leaflet with React
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
+
+const LocationMarker = () => {
+  const [position, setPosition] = useState(null);
+  const map = useMap();
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          const pos = [latitude, longitude];
+          setPosition(pos);
+          map.setView(pos, map.getZoom());
+        },
+        (e) => {
+          console.log(e);
+          console.error("Error: The Geolocation service failed.");
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0,
+        },
+      );
+    } else {
+      console.error("Error: Your browser doesn't support geolocation.");
+    }
+  }, [map, navigator]);
+
+  return position === null ? null : <Marker position={position} />;
+};
+
+const MapComponent = () => {
+  const initialPosition = [51.505, -0.09]; // Default position
+
+  return (
+    <MapContainer
+      center={initialPosition}
+      zoom={13}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <LocationMarker />
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
